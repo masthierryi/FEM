1ed2adbb809f08cba32a31087ae719dd9b94e944
masTHIERRYi <thierrymiqueias@gmail.com>
2025-03-22 19:10:59 -0300

    reth

--- menu.m
+++ menu.m
@@ -77,78 +77,6 @@ data(inp).BC = [ 1      1 1 ;  % at node 1, BC is
 % ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨
 % -------------------------------------------------------------------------
 
-% % Input 2
-% % -------------------------------------------------------------------------
-% inp = 2;
-% FEM parameters  ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨
-% data(inp).n_el = 1;
-% lt = data(inp).n_el; 
-% 
-% % beam parameters ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨
-% data(inp).L = 1; 
-% 
-% data(inp).rho = [7850,lt];
-% 
-% data(inp).E = [210e9, lt];
-% 
-% data(inp).nu = [0.3, lt];
-% % ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨
-% 
-% % cross-section data  ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ 
-% data(inp).d = [0.5, 0.5, 1, lt];
-% 
-% data(inp).geo = 2;
-% % ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨
-% 
-% % multilayer data ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨
-% data(inp).layer = [0.04, 5000, 20e9, 0.3, 1, lt];
-% % ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨
-% 
-% % boundary conditions ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨
-% % for coupling beams, the last node of actual beam must to be free 1 1, and 
-% % the first node of the next beam must to be campled 0 0, but the code does
-% % this automatically, so let it free 1 1
-% lno = data(inp).n_el+1;
-% data(inp).BC = [ 1 1 1 ; % at node 1, BC is 
-%                lno 0 0 ]; % x = L 
-% % ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨
-% % -------------------------------------------------------------------------
-
-% % TRUSS  
-% % -------------------------------------------------------------------------
-% % FEM parameters  ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ 
-% % [element, initial node, final node , dof type*]
-% %*element type: 1- bar, 2- beam
-% data.t_el = [1 1 3 2;
-%              2 2 3 2];
-% 
-% % node coordinates
-% data.n_c = [0, 50; % node 1                 
-%             50, 50; % node 2
-%             100, 0];% node 3
-% % ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨
-% 
-% % beam parameters ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨
-% % for each node
-% data.rho = [7850];
-% data.E = [210e9];
-% data.poisson = [0.3];
-% % ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨
-% 
-% % cross-section data  ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨
-% data.CS_form = [2]; % circle
-% data.CS_dim = [0.125,0.125]; 
-% % ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨
-% 
-% % boundary conditions ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨
-% % [node where it is applied, sheer stress parametrer, momentum parametrer];
-% % 1 = free; 0 = restrained
-% data.BC = [ 1 1 0 ;  % at node 1, BC is 
-%             2 1 0 ]; % x = L 
-% % ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨ ¨
-% % -------------------------------------------------------------------------
-% _________________________________________________________________________
-
 % _________________________________________________________________________
 % For each beam provided in the pages of the data (array inside the array), 
 % its matrix are calculated individually. Then, these matrices are assembled 
@@ -174,15 +102,6 @@ type = 3;
 % _________________________________________________________________________
 
 toc()
-
-% 
-% fprintf("\nU disp| %+e| %+e| %+e| %+e| \n",FEA.U_disp(3,3),FEA.U_disp(12,11),FEA.U_disp(5,18),FEA.U_disp(19,36));
-% fprintf("U rot_| %+e| %+e| %+e| %+e| \n",FEA.U_rot(3,3),FEA.U_rot(12,11),FEA.U_rot(5,18),FEA.U_rot(19,36));
-% fprintf("K_____| %+e| %+e| %+e| %+e| \n",FEA.matrices.CK(3,3),FEA.matrices.CK(25,23),FEA.matrices.CK(32,29),FEA.matrices.CK(32,30));
-% fprintf("M_____| %+e| %+e| %+e| %+e| \n",FEA.matrices.CM(3,3),FEA.matrices.CM(25,23),FEA.matrices.CM(32,29),FEA.matrices.CM(32,30));
-% fprintf("freq__| %+e| %+e| %+e| %+e| \n",FEA.result.natfreq(1),FEA.result.natfreq(2),FEA.result.natfreq(3),FEA.result.natfreq(5));
-% fprintf("beam__| A = %+g| I = %+g| k = %+g| G = %+g| \n", FEA.Beam.A(1),FEA.Beam.I(1),FEA.Beam.k(1),FEA.Beam.G(1));
-% fprintf("beam__| r_e = %+g| s_e = %+g| S = %+g| gama = %+g|",FEA.Beam.r_e(1),FEA.Beam.s_e(1),FEA.S,FEA.gama);
 
 % 
 % time chart --------------------------------------------------------------
